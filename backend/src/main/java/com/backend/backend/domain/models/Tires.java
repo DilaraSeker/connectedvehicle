2978609245c196393fef116c79916ba974ff4d07
DilaraSeker <dlrseker@gmail.com>
2021-06-23 01:43:24 +0300

    Create Tires.java

---
+++ backend/src/main/java/com/backend/backend/domain/models/Tires.java
@@ -0,0 +1,40 @@
+package com.backend.backend.domain.models;
+
+public class Tires {
+    TirePressureStatus tirePressureStatusPressureFrontLeft;
+    TirePressureStatus tirePressureStatusPressureFrontRight;
+    TirePressureStatus tirePressureStatusPressureRearLeft;
+    TirePressureStatus tirePressureStatusPressureRearRight;
+
+    public TirePressureStatus getTirePressureStatusPressureFrontLeft() {
+        return tirePressureStatusPressureFrontLeft;
+    }
+
+    public void setTirePressureStatusPressureFrontLeft(TirePressureStatus tirePressureStatusPressureFrontLeft) {
+        this.tirePressureStatusPressureFrontLeft = tirePressureStatusPressureFrontLeft;
+    }
+
+    public TirePressureStatus getTirePressureStatusPressureFrontRight() {
+        return tirePressureStatusPressureFrontRight;
+    }
+
+    public void setTirePressureStatusPressureFrontRight(TirePressureStatus tirePressureStatusPressureFrontRight) {
+        this.tirePressureStatusPressureFrontRight = tirePressureStatusPressureFrontRight;
+    }
+
+    public TirePressureStatus getTirePressureStatusPressureRearLeft() {
+        return tirePressureStatusPressureRearLeft;
+    }
+
+    public void setTirePressureStatusPressureRearLeft(TirePressureStatus tirePressureStatusPressureRearLeft) {
+        this.tirePressureStatusPressureRearLeft = tirePressureStatusPressureRearLeft;
+    }
+
+    public TirePressureStatus getTirePressureStatusPressureRearRight() {
+        return tirePressureStatusPressureRearRight;
+    }
+
+    public void setTirePressureStatusPressureRearRight(TirePressureStatus tirePressureStatusPressureRearRight) {
+        this.tirePressureStatusPressureRearRight = tirePressureStatusPressureRearRight;
+    }
+}
